d23ff6a7303b437c6e6ffa0579c8cf2058af2c56
Ari Sweedler <ari@sweedler.com>
2019-12-13 17:37:03 -0800

    uhhh shabbat shalom brothers

--- challenge8/digital.c
+++ challenge8/digital.c
@@ -3,28 +3,35 @@
 int main(int argc, char** argv) {
     char file[15002];
     read(STDIN_FILENO, file, 15001);
-    file[15001] = '\0';
-    int min0s = sizeof(Layer) + 1;
-    int answer = 0;
+
+    char image[sizeof(Layer)];
+    for (int i = 0; i < sizeof(Layer); i++) image[i] = '2';
+
     for (int layer = 0; layer < 100; layer++) {
-        int counter[3] = {0, 0, 0};
         for (int i = 0; i < sizeof(Layer); i++) {
-            char c = file[layer*sizeof(Layer) + i];
-            int char_code = c - '0';
-            counter[char_code]++;
-        }
-        if (counter[0] < min0s) {
-            printf("Layer %d has %d zeros\n", layer, counter[0]);
-            min0s = counter[0];
-            answer = counter[1] * counter[2];
-            printf("\tAnswer for this layer is %d\n", answer);
+            if (image[i] == '2') {
+                image[i] = file[layer*sizeof(Layer) + i];
+            }
         }
     }
 
-    printf("Answer is: %d\n", answer);
+    for (int i = 0; i < sizeof(Layer); i++) {
+        printf("%c", image[i]);
+        if ((i+1)%25 == 0) printf("\n");
+    }
+
     return 0;
 }
 
-// the Elves would like you to find the layer that contains the fewest 0 digits.
-// On that layer, what is the number of 1 digits multiplied by the number of 2
-// digits?
+// 0 is black, 1 is white, and 2 is transparent.
+// Algorithm: read each layr (starting with top layer)
+// * If there's a pixel there already, continue
+// * If there's not yet, then add this. (unless it's 2)
+//
+// Answer:
+// 1001001100011001111011110
+// 1001010010100101000010000
+// 1111010000100001110011100
+// 1001010000101101000010000
+// 1001010010100101000010000
+// 1001001100011101000011110
